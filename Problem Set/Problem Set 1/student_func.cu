82f2da3e774dc7a5da4057e11aa910b56871a970
AIxi0425 <aixi0425@gmail.com>
2016-10-29 16:33:26 +0800

    update

--- Problem Set/Problem Set 1/student_func.cu
+++ Problem Set/Problem Set 1/student_func.cu
@@ -67,7 +67,7 @@ void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_r
 {
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
-  const dim3 blockSize(ceil((numCols+15)/16), ceil((numRows+15)/16), 1);  //TODO
+  const dim3 blockSize(ceil(numCols/16), ceil(numRows/16), 1);  //TODO
   const dim3 gridSize( 16, 16, 1);  //TODO
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
