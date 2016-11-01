1fb3460d0deaecb44ade28ca78690d4762c1c1c7
AIxi0425 <aixi0425@gmail.com>
2016-11-01 23:10:12 +0800

    update cuda_memory.cu

--- cuda_memory/cuda_memory.cu
+++ cuda_memory/cuda_memory.cu
@@ -31,5 +31,25 @@ int main(int argc, char** argv)
 	 */
 	use_local_memory_GPU<<<1, 128>>>(2.0f);
 	
+	/*
+	 * Next, call a kernel that shows using global memory
+	 */
+	float h_arr[128]; // convention: h_ variables live on host
+	float *d_arr;	  // convention: d_ variables live on device (GPU global mem)  
+	
+	// allocate global memory on the device, place result in "d_arr"
+	cudaMalloc((void**)&d_arr, sizeof(float) * 128);
+	// now copy data from host memory to device memory
+	cudaMemcpy((void *)d_arr, (void*)h_arr, sizeof(float) * 128, cudaMemcpyHostToDevice);
+	// launch the kernel 
+	use_global_memory_GPU<<<1, 128>>>(d_arr);
+	// copy the modified array back to the host, overwriting contents of h_arr
+	cudaMemcpy((void *)h_arr, (void *)d_arr, sizeof(float) * 128, cudaMemcpyDeviceToHost);
+	// ... do other stuff ...
+	for (int i = 0; i < 128; ++i)
+	{
+		printf("%f", h_arr[i]);
+		printf((i % 4 != 3) ? "\t" : "\n");
+	}
 	return 0;
 }
